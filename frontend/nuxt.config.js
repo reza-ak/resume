8da354b34d6d78567b30e4d584c330d6bee28a3d
reza-ak <reeza.dev@gmail.com>
2022-03-21 22:16:20 +0330

    set title and description in config.nuxt.js

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,14 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'frontend',
+    title: 'Reeza.dev',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: "Hi, my name is Reza and I am a full stack web developer"},
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
